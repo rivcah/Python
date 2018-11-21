531033b4273337cffb291c330ab66d23356f3b51
Rivca E Hildebrandt <rivca.hildebrandt@gmail.com>
2018-11-21 13:07:12 +0200

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,27 @@
+##This function displays a monthly calendar
+
+from calendar import *
+def cal():
+	month_year = {
+                'January' : '1',
+                'February' : '2',
+                'March' : '3',
+                'April' : '4',
+                'May' : '5',
+                'June' : '6',
+                'July' : '7',
+                'August' : '8',
+                'September' : '9',
+                'October' : '10',
+                'November' : '11',
+                'December' : '12'}
+
+
+	y = int(input("Choose a year: \n"))
+	m = input("Choose a month: \n")
+	m = int(month_year[m])
+
+	cal = month(y, m)
+
+	print("\n \n This is your calendar:\n")
+	print(cal)
